1b58906b76abc93cd45dd73c0d6f103ad4d86d51
mdmorshedsagar <mdmosahidsagar5304257@gmail.com>
2023-06-09 07:49:09 -1200

    popular-class-api

--- index.js
+++ index.js
@@ -1,58 +1,73 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
-
-// middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
-
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3uy5gri.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
 async function run() {
   try {
-    await client.connect();
+    const client = new MongoClient(uri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      },
+    });
 
-    const usersCollection = client.db("summer_camp_db").collection("users");
+    await client.connect();
+    console.log('Connected to MongoDB');
+
+    const db = client.db('summer_camp_db');
+    const usersCollection = db.collection('users');
+    const classesCollection = db.collection('classes');
 
     app.post('/users', async (req, res) => {
-        const user = req.body;
-        const query = { email: user.email }
-        const existingUser = await usersCollection.findOne(query);
-  
-        if (existingUser) {
-          return res.send({ message: 'user already exists' })
-        }
-  
-        const result = await usersCollection.insertOne(user);
-        res.send(result);
-      });
+      const user = req.body;
+      console.log('New user:', user);
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    await client.close();
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: 'User already exists' });
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get('/popular_classes', async (req, res) => {
+      try {
+        const popularClasses = await classesCollection
+          .find()
+          .sort({ studentsCount: -1 })
+          .limit(6)
+          .toArray();
+
+        res.json(popularClasses);
+      } catch (error) {
+        console.error('Error retrieving popular classes:', error);
+        res.status(500).json({ error: 'Server error' });
+      }
+    });
+
+    app.get('/', (req, res) => {
+      res.send('Hello World!');
+    });
+
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
   }
 }
+
 run().catch(console.dir);
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
